48dee5810847d31a03ed667e99b82d576d437961
satpreet07 <satpreetvirdi007@gmail.com>
2022-12-15 01:45:45 +0530

    bug resolved

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,33 +1,30 @@
-import React, { useEffect } from 'react'
-import Codemirror from 'codemirror'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/dracula.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-
+import React, { useEffect, useRef } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 
 const Editor = () => {
-  // useEffect(() => {
-  //   async function init() {
-  //     Codemirror.fromTextArea(document.getElementById('realtimeEditor'),
-  //       {
-  //         mode: {
-  //           name: 'javascript', json: true
-  //         },
-  //         theme: 'dracula',
-  //         autoCloseTags: true,
-  //         autoCloseBrackets: true,
-  //         lineNumbers: true
-  //       }
-  //       );
-  //   }
-  //   init(); 
-  // }, []);
+  const editorRef = useRef(null);
+  useEffect(() => {
+    async function init() {
+          editorRef.current  = Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+      editorRef.current.on('change', (instance, changes) => {
+        console.log('change', changes);
+      });
+    }
+    init();
+  }, []);
 
-  return <textarea id='realtimeEditor'>
-
-  </textarea>
+  return <textarea id="realtimeEditor"></textarea>;
 };
 
-export default Editor 
+export default Editor;
